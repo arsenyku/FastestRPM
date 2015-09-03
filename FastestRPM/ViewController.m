919934dde3971d3347bad492bc2cb19e346cad72
asu <arsenyk.ustaris.dev@gmail.com>
2015-09-03 14:55:33 -0700

    Shows dial, pointer, with button to rotate pointer.

--- FastestRPM/ViewController.m
+++ FastestRPM/ViewController.m
@@ -10,6 +10,10 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) UIView *pointerContainerView;
+@property (weak, nonatomic) IBOutlet UIImageView *pointerImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *dialImageView;
+@property (assign, nonatomic) float currentAngle;
 @end
 
 @implementation ViewController
@@ -17,11 +21,244 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+
+    self.pointerImageView.alpha = 1.0;
+    self.pointerImageView.translatesAutoresizingMaskIntoConstraints = NO;
+
+    self.pointerContainerView = [[UIView alloc] initWithFrame:self.dialImageView.frame];
+    self.pointerContainerView.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [self.pointerContainerView addSubview:self.pointerImageView];
+    
+    [self.view addSubview:self.pointerContainerView];
+    
+    [self.view addConstraints:[self createDialConstraints] ];
+    [self.view addConstraints:[self createPointerConstraints] ];
+    
+    self.currentAngle = 490.0;
+    [self rotateView:self.pointerContainerView byDegrees:self.currentAngle];
+ }
+
+
+-(NSArray*)createDialConstraints{
+    NSMutableArray* result = [NSMutableArray new];
+    
+    float sideLength = MIN(self.view.frame.size.height, self.view.frame.size.width) - 10;
+    
+    
+    [result addObject:[NSLayoutConstraint constraintWithItem:self.dialImageView
+                                                   attribute:NSLayoutAttributeHeight
+                                                   relatedBy:NSLayoutRelationEqual
+                                                      toItem:nil
+                                                   attribute:NSLayoutAttributeNotAnAttribute
+                                                  multiplier:1.0
+                                                    constant:sideLength]];
+    
+    [result addObject:[NSLayoutConstraint constraintWithItem:self.dialImageView
+                                                   attribute:NSLayoutAttributeWidth
+                                                   relatedBy:NSLayoutRelationEqual
+                                                      toItem:nil
+                                                   attribute:NSLayoutAttributeNotAnAttribute
+                                                  multiplier:1.0
+                                                    constant:sideLength]];
+    
+    return result;
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(NSArray*)createPointerConstraints{
+	NSMutableArray* result = [NSMutableArray new];
+
+    float horizontalOffset = -25;
+    
+    [result addObject:[NSLayoutConstraint constraintWithItem:self.pointerImageView
+                                                   attribute:NSLayoutAttributeRight
+                                                   relatedBy:NSLayoutRelationEqual
+                                                      toItem:self.pointerContainerView
+                                                   attribute:NSLayoutAttributeRight
+                                                  multiplier:1.0
+                                                    constant:horizontalOffset]];
+    
+    [result addObject:[NSLayoutConstraint constraintWithItem:self.pointerImageView
+                                                   attribute:NSLayoutAttributeLeft
+                                                   relatedBy:NSLayoutRelationEqual
+                                                      toItem:self.pointerContainerView
+                                                   attribute:NSLayoutAttributeCenterX
+                                                  multiplier:1.0
+                                                    constant:horizontalOffset]];
+    
+    [result addObject:[NSLayoutConstraint constraintWithItem:self.pointerImageView
+                                                   attribute:NSLayoutAttributeCenterY
+                                                   relatedBy:NSLayoutRelationEqual
+                                                      toItem:self.pointerContainerView
+                                                   attribute:NSLayoutAttributeCenterY
+                                                  multiplier:1.0
+                                                    constant:+8]];
+    
+    [result addObject:[NSLayoutConstraint constraintWithItem:self.pointerImageView
+                                                   attribute:NSLayoutAttributeHeight
+                                                   relatedBy:NSLayoutRelationEqual
+                                                      toItem:nil
+                                                   attribute:NSLayoutAttributeNotAnAttribute
+                                                  multiplier:1.0
+                                                    constant:50.0]];
+    
+
+    
+    [result addObject:[NSLayoutConstraint constraintWithItem:self.pointerContainerView
+                                                   attribute:NSLayoutAttributeCenterX
+                                                   relatedBy:NSLayoutRelationEqual
+                                                      toItem:self.dialImageView
+                                                   attribute:NSLayoutAttributeCenterX
+                                                  multiplier:1.0
+                                                    constant:0.0]];
+    
+    [result addObject:[NSLayoutConstraint constraintWithItem:self.pointerContainerView
+                                                   attribute:NSLayoutAttributeCenterY
+                                                   relatedBy:NSLayoutRelationEqual
+                                                      toItem:self.dialImageView
+                                                   attribute:NSLayoutAttributeCenterY
+                                                  multiplier:1.0
+                                                    constant:0.0]];
+    
+    [result addObject:[NSLayoutConstraint constraintWithItem:self.pointerContainerView
+                                                   attribute:NSLayoutAttributeHeight
+                                                   relatedBy:NSLayoutRelationEqual
+                                                      toItem:self.dialImageView
+                                                   attribute:NSLayoutAttributeHeight
+                                                  multiplier:1.0
+                                                    constant:0.0]];
+    
+    [result addObject:[NSLayoutConstraint constraintWithItem:self.pointerContainerView
+                                                   attribute:NSLayoutAttributeWidth
+                                                   relatedBy:NSLayoutRelationEqual
+                                                      toItem:self.dialImageView
+                                                   attribute:NSLayoutAttributeWidth
+                                                  multiplier:1.0
+                                                    constant:0.0]];
+    
+    
+    return [result copy];
 }
+
+-(void)matchView:(UIView*)view dimensionsAndPositionWithSourceView:(UIView*)sourceView{
+    view.frame = CGRectMake(sourceView.frame.origin.x, sourceView.frame.origin.y,
+                            sourceView.frame.size.width, sourceView.frame.size.height);
+};
+
+- (IBAction)addTenDegrees:(id)sender {
+    self.currentAngle += 10.0;
+//    self.pointerImageView.image = [self imageFromImage:self.pointerImageView.image
+//                                      rotatedByDegrees:self.currentAngle];
+    [self rotateView:self.pointerContainerView byDegrees:self.currentAngle];
+}
+
+
+-(void)rotateView:(UIView*)view byDegrees:(float)angle{
+    float radians = [self radiansFromDegrees:self.currentAngle];
+    NSLog(@"%f rad == %f deg", radians, self.currentAngle);
+    CGAffineTransform transform = CGAffineTransformMakeRotation(radians);
+    view.transform = transform;
+}
+
+
+-(float)radiansFromDegrees:(float)degrees{
+    return ((degrees) * (M_PI / 180.0));
+    
+}
+
+
+-(UIImage*)image:(UIImage*)original drawnIntoRect:(CGRect)rectangle xOffset:(float)xOffset yOffset:(float)yOffset{
+
+    
+    UIGraphicsBeginImageContext(rectangle.size);
+    
+    CGContextRef bitmap = UIGraphicsGetCurrentContext();
+    
+    CGRect targetRectangle = CGRectMake(xOffset, yOffset, original.size.width, original.size.height);
+    
+    //CGContextRotateCTM (context, [self radiansFromDegrees:angle]);
+    //CGContextTranslateCTM (context, -10, 0);
+    //[original drawInRect:rectangle];
+    CGContextDrawImage(bitmap, targetRectangle, [original CGImage]);
+    
+    // Retrieve the image and end the context
+    
+    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+    
+    
+}
+
+
+- (UIImage *)imageFromImage:(UIImage*)original rotatedByDegrees:(CGFloat)degrees
+{
+    // calculate the size of the rotated view's containing box for our drawing space
+    UIView *rotatedViewBox = [[UIView alloc] initWithFrame:CGRectMake(0,0,original.size.width, original.size.height)];
+    CGAffineTransform t = CGAffineTransformMakeRotation([self radiansFromDegrees:degrees]);
+    rotatedViewBox.transform = t;
+    CGSize rotatedSize = rotatedViewBox.frame.size;
+    //[rotatedViewBox release];
+    
+    // Create the bitmap context
+    UIGraphicsBeginImageContext(rotatedSize);
+    CGContextRef bitmap = UIGraphicsGetCurrentContext();
+    
+    // Move the origin to the middle of the image so we will rotate and scale around the center.
+    CGContextTranslateCTM(bitmap, rotatedSize.width/2, rotatedSize.height/2);
+    
+    //   // Rotate the image context
+    CGContextRotateCTM(bitmap, [self radiansFromDegrees:degrees]);
+    
+    // Now, draw the rotated/scaled image into the context
+    CGContextScaleCTM(bitmap, 1.0, -1.0);
+    CGContextDrawImage(bitmap, CGRectMake(-original.size.width / 2,
+                                          -original.size.height / 2,
+                                          original.size.width,
+                                          original.size.height),
+                       			[original CGImage]);
+    
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
+    
+}
+
+
+
+-(UIImage*)rotateImage:(UIImage*)image
+      byAngleInDegrees:(float)angle
+
+{
+    
+    UIGraphicsBeginImageContextWithOptions(image.size, YES, 0.0f);
+    
+    
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGContextRotateCTM (context, [self radiansFromDegrees:angle]);
+    //CGContextTranslateCTM (context, -10, 0);
+    
+    
+    [image drawInRect:CGRectMake(0,0,image.size.width,image.size.height)
+            blendMode:kCGBlendModeScreen
+                alpha:1.0f];
+    
+    // Retrieve the image and end the context
+    
+    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+    
+}
+
+
 
 @end
