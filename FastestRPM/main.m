e3e4d21e255963722f3b42c933b50630ff3c6b27
asu <arsenyk.ustaris.dev@gmail.com>
2015-09-03 08:51:30 -0700

    Initial Commit

---
+++ FastestRPM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FastestRPM
+//
+//  Created by asu on 2015-09-03.
+//  Copyright (c) 2015 asu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
