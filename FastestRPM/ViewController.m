667764a495c50c41cc12d617e9e3f7f791082cb6
asu <arsenyk.ustaris.dev@gmail.com>
2015-09-03 22:10:58 -0700

    Fixes constraints so that pointer is properly aligned to centre of dial.  Adds outlet to button for calibrating the pointer.  Disables Autoresizingmaskcontraints for pertinent controls.  Adds placeholder constraints in Storyboard.

--- FastestRPM/ViewController.m
+++ FastestRPM/ViewController.m
@@ -14,32 +14,40 @@
 @property (weak, nonatomic) IBOutlet UIImageView *pointerImageView;
 @property (weak, nonatomic) IBOutlet UIImageView *dialImageView;
 @property (assign, nonatomic) float currentAngle;
+@property (weak, nonatomic) IBOutlet UIButton *calibrationButton;
 @end
 
 @implementation ViewController
 
-static float const MinAngle = 230.0f;  // -130 deg
+static float const AngleForMinPosition = 222.0f;  // degrees
+static float const AngleForMaxPosition = 43.0;    // degrees
 
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 
+    BOOL calibrationMode = NO;
+	self.calibrationButton.hidden = !calibrationMode ;
+    self.calibrationButton.alpha = calibrationMode ? 1.0 : 0.0;
 
     self.pointerImageView.alpha = 1.0;
-    self.pointerImageView.translatesAutoresizingMaskIntoConstraints = NO;
 
     self.pointerContainerView = [[UIView alloc] initWithFrame:self.dialImageView.frame];
-    self.pointerContainerView.translatesAutoresizingMaskIntoConstraints = NO;
     
+    self.pointerImageView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.pointerContainerView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.dialImageView.translatesAutoresizingMaskIntoConstraints = NO;
+
     [self.pointerContainerView addSubview:self.pointerImageView];
     
     [self.view addSubview:self.pointerContainerView];
     
     [self.view addConstraints:[self createDialConstraints] ];
     [self.view addConstraints:[self createPointerConstraints] ];
-    
+
     [self resetMeter:nil];
+
 }
 
 
@@ -81,7 +89,7 @@ static float const MinAngle = 230.0f;  // -130 deg
 
 -(void)resetMeter:(id)sender{
 
-    self.currentAngle = 130.0f;
+    self.currentAngle = 360 - AngleForMinPosition;
 
     if (sender == nil){
         [self rotateView:self.pointerContainerView byDegrees:self.currentAngle];
@@ -97,13 +105,12 @@ static float const MinAngle = 230.0f;  // -130 deg
     float const minVelocity = 0;
     float const maxVelocity = 5000;
     
-    float const maxAngle = -40;  // 320 deg
     
     float velocityRange = maxVelocity - minVelocity;
-    float angleRange = fabs(maxAngle - MinAngle);
+    float angleRange = fabs(AngleForMaxPosition - AngleForMinPosition);
     
     float angle = (velocity / velocityRange) * angleRange;
-    return angle - MinAngle;
+    return angle - AngleForMinPosition;
     
 }
 
@@ -130,6 +137,22 @@ static float const MinAngle = 230.0f;  // -130 deg
                                                    attribute:NSLayoutAttributeNotAnAttribute
                                                   multiplier:1.0
                                                     constant:sideLength]];
+   
+    [result addObject:[NSLayoutConstraint constraintWithItem:self.dialImageView
+                                                   attribute:NSLayoutAttributeCenterX
+                                                   relatedBy:NSLayoutRelationEqual
+                                                      toItem:self.view
+                                                   attribute:NSLayoutAttributeCenterX
+                                                  multiplier:1.0
+                                                    constant:0.0]];
+    
+    [result addObject:[NSLayoutConstraint constraintWithItem:self.dialImageView
+                                                   attribute:NSLayoutAttributeCenterY
+                                                   relatedBy:NSLayoutRelationEqual
+                                                      toItem:self.view
+                                                   attribute:NSLayoutAttributeCenterY
+                                                  multiplier:1.0
+                                                    constant:0.0]];
     
     return result;
 
@@ -138,15 +161,46 @@ static float const MinAngle = 230.0f;  // -130 deg
 -(NSArray*)createPointerConstraints{
 	NSMutableArray* result = [NSMutableArray new];
 
-    float horizontalOffset = -25;
+    [result addObject:[NSLayoutConstraint constraintWithItem:self.pointerContainerView
+                                                   attribute:NSLayoutAttributeCenterX
+                                                   relatedBy:NSLayoutRelationEqual
+                                                      toItem:self.dialImageView
+                                                   attribute:NSLayoutAttributeCenterX
+                                                  multiplier:1.0
+                                                    constant:0.0]];
     
+    [result addObject:[NSLayoutConstraint constraintWithItem:self.pointerContainerView
+                                                   attribute:NSLayoutAttributeCenterY
+                                                   relatedBy:NSLayoutRelationEqual
+                                                      toItem:self.dialImageView
+                                                   attribute:NSLayoutAttributeCenterY
+                                                  multiplier:1.0
+                                                    constant:8.0]];
+    
+    [result addObject:[NSLayoutConstraint constraintWithItem:self.pointerContainerView
+                                                   attribute:NSLayoutAttributeHeight
+                                                   relatedBy:NSLayoutRelationEqual
+                                                      toItem:self.dialImageView
+                                                   attribute:NSLayoutAttributeHeight
+                                                  multiplier:1.0
+                                                    constant:0.0]];
+    
+    [result addObject:[NSLayoutConstraint constraintWithItem:self.pointerContainerView
+                                                   attribute:NSLayoutAttributeWidth
+                                                   relatedBy:NSLayoutRelationEqual
+                                                      toItem:self.dialImageView
+                                                   attribute:NSLayoutAttributeWidth
+                                                  multiplier:1.0
+                                                    constant:0.0]];
+    
+
     [result addObject:[NSLayoutConstraint constraintWithItem:self.pointerImageView
                                                    attribute:NSLayoutAttributeRight
                                                    relatedBy:NSLayoutRelationEqual
                                                       toItem:self.pointerContainerView
                                                    attribute:NSLayoutAttributeRight
                                                   multiplier:1.0
-                                                    constant:horizontalOffset]];
+                                                    constant:0]];
     
     [result addObject:[NSLayoutConstraint constraintWithItem:self.pointerImageView
                                                    attribute:NSLayoutAttributeLeft
@@ -154,7 +208,7 @@ static float const MinAngle = 230.0f;  // -130 deg
                                                       toItem:self.pointerContainerView
                                                    attribute:NSLayoutAttributeCenterX
                                                   multiplier:1.0
-                                                    constant:horizontalOffset]];
+                                                    constant:-25.0]];
     
     [result addObject:[NSLayoutConstraint constraintWithItem:self.pointerImageView
                                                    attribute:NSLayoutAttributeCenterY
@@ -162,7 +216,7 @@ static float const MinAngle = 230.0f;  // -130 deg
                                                       toItem:self.pointerContainerView
                                                    attribute:NSLayoutAttributeCenterY
                                                   multiplier:1.0
-                                                    constant:+8]];
+                                                    constant:0.0]];
     
     [result addObject:[NSLayoutConstraint constraintWithItem:self.pointerImageView
                                                    attribute:NSLayoutAttributeHeight
@@ -172,48 +226,14 @@ static float const MinAngle = 230.0f;  // -130 deg
                                                   multiplier:1.0
                                                     constant:50.0]];
     
-
-    
-    [result addObject:[NSLayoutConstraint constraintWithItem:self.pointerContainerView
-                                                   attribute:NSLayoutAttributeCenterX
-                                                   relatedBy:NSLayoutRelationEqual
-                                                      toItem:self.dialImageView
-                                                   attribute:NSLayoutAttributeCenterX
-                                                  multiplier:1.0
-                                                    constant:0.0]];
-    
-    [result addObject:[NSLayoutConstraint constraintWithItem:self.pointerContainerView
-                                                   attribute:NSLayoutAttributeCenterY
-                                                   relatedBy:NSLayoutRelationEqual
-                                                      toItem:self.dialImageView
-                                                   attribute:NSLayoutAttributeCenterY
-                                                  multiplier:1.0
-                                                    constant:0.0]];
-    
-    [result addObject:[NSLayoutConstraint constraintWithItem:self.pointerContainerView
-                                                   attribute:NSLayoutAttributeHeight
-                                                   relatedBy:NSLayoutRelationEqual
-                                                      toItem:self.dialImageView
-                                                   attribute:NSLayoutAttributeHeight
-                                                  multiplier:1.0
-                                                    constant:0.0]];
-    
-    [result addObject:[NSLayoutConstraint constraintWithItem:self.pointerContainerView
-                                                   attribute:NSLayoutAttributeWidth
-                                                   relatedBy:NSLayoutRelationEqual
-                                                      toItem:self.dialImageView
-                                                   attribute:NSLayoutAttributeWidth
-                                                  multiplier:1.0
-                                                    constant:0.0]];
-    
-    
     return [result copy];
 }
 
 
 - (IBAction)addTenDegrees:(id)sender {
-    self.currentAngle += 10.0;
+    self.currentAngle += 10;
     [self rotateView:self.pointerContainerView byDegrees:self.currentAngle];
+    NSLog(@"angle: %f", self.currentAngle);
 }
 
 
